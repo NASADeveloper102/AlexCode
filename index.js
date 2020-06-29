bc6f8c9b75f1192a304e6b066535a532b01cd6bb
NASADeveloper102 <NASADeveloper102@gmail.com>
2020-06-29 09:52:43 -0400

    Refactored, a lot

--- index.js
+++ index.js
@@ -1,108 +1,164 @@
-
-let VariableArray = new Array(10);
+let BufferArray = [];
+let OpenWhileLoopIDs = [];
+let ClosedWhileLoopIDs = [];
+let IfIDs = [];
 let CurrentIndex = 0;
-let CopiedPointer = null;
-let CurrentString = "";
-for( let i = 0 ; i < VariableArray.length; i++ ){
-    VariableArray[i] = 0;
-}
-const ParseAndExecute = ( AlexCodeInput ) => {
-    
-    VariableArray = new Array(10);
-    CurrentIndex = 0;
-    CopiedPointer = null;
-    CurrentString = "";
-    document.getElementById('Output').value = "";
-    for( let i = 0 ; i < VariableArray.length; i++ ){
-        VariableArray[i] = 0;
+let CopiedIndex = undefined;
+let CodeIndex = 0;
+let CodeString;
+let codeInterval = undefined;
+let DrawObject = {};
+let Vertices = 0;
+let CurrentDraw = "";
+const Clear = () => {
+    BufferArray = [];
+    for(let i = 0; i < 10; i++){
+        BufferArray.push(0);
     }
-    let EvalString = "";
-    for( let i = 0; i < AlexCodeInput.length; i++ ){
-        if(AlexCodeInput[i] == "+" || AlexCodeInput[i] == "a"){
-            EvalString += "VariableArray[CurrentIndex]++;";
-        }
-        if(AlexCodeInput[i] == "-" || AlexCodeInput[i] == 's'){
-            EvalString += "VariableArray[CurrentIndex]--;";
-        }
-        if(AlexCodeInput[i] == "<" || AlexCodeInput[i] == 'l'){
-            EvalString += "if(CurrentIndex == 0){CurrentIndex = VariableArray.length;} else {CurrentIndex--;}";
-        }
-        if(AlexCodeInput[i] == ">" || AlexCodeInput[i] == "r"){
-            EvalString += "if(CurrentIndex == VariableArray.length - 1){CurrentIndex = 0;} else{CurrentIndex++;}";
-        }
-        if(AlexCodeInput[i] == "["){
-            EvalString += "while(VariableArray[CurrentIndex] != 1){";
-        }
-        if(AlexCodeInput[i] == ']'){
-            EvalString += "}";
-        }
-        if(AlexCodeInput[i] == '.' || AlexCodeInput[i] == "o"){
-            EvalString += "CurrentString += (String.fromCharCode(VariableArray[CurrentIndex]));";
-        }
-        if(AlexCodeInput[i] == ','){
-            console.error('Not implimented yet');
-        }
-        if(AlexCodeInput[i] == '~'){
-            EvalString += "CopiedPointer = CurrentIndex;";
-        }
-        if(AlexCodeInput[i] == "{"){
-            console.error('Not implimented yet');
-        }
-        if(AlexCodeInput[i] == "}"){
-            console.error("Not implimented yet");
-        }
-        if(AlexCodeInput[i] == "#"){
-            EvalString += "VariableArray[CurrentIndex] += VariableArray[CopiedPointer];"
-        }
-        if(AlexCodeInput[i] == ";"){
-            console.error("Not implimented yet");
-        }
-        if(AlexCodeInput[i] == "$"){
-            EvalString += "VariableArray = new Array(VariableArray[CurrentIndex]); for(let i = 0; i < VariableArray.length; i++){VariableArray[i] = 0;}";
-        }
-        if(AlexCodeInput[i] == "!"){
-            EvalString += "console.error(VariableArray[CurrentIndex]);";
-        }
-        if(AlexCodeInput[i] == "?"){
-            EvalString += "VariableArray[CurrentIndex] -= VariableArray[CopiedPointer];";
-        }
-        if(AlexCodeInput[i] == "*" || AlexCodeInput[i] == 'x'){
-            EvalString += "VariableArray[CurrentIndex] *= VariableArray[CopiedPointer];";
-        }
-        if(AlexCodeInput[i] == "&"){
-            EvalString += "document.getElementById('Output').value = '';";
-        }
-        if(AlexCodeInput[i] == "p"){
-            EvalString += "document.getElementById('Output').value += CurrentString + '\\n'; CurrentString = '';"
-        }
-        if(AlexCodeInput[i] == "^"){
-            EvalString += "if(VariableArray[CopiedPointer] == 0){if(CurrentIndex + 1 != VariableArray.length){VariableArray[CurrentIndex + 1] = Math.sin(VariableArray[CurrentIndex]);}else{VariableArray[0] = Math.sin(VariableArray[CurrentIndex]);}}if(VariableArray[CopiedPointer] == 1){if(CurrentIndex + 1 != VariableArray.length){VariableArray[CurrentIndex + 1] = Math.cos(VariableArray[CurrentIndex]);}else{VariableArray[0] = Math.cos(VariableArray[CurrentIndex]);}}if(VariableArray[CopiedPointer] == 2){if(CurrentIndex + 1 != VariableArray.length){VariableArray[CurrentIndex + 1] = Math.tan(VariableArray[CurrentIndex]);}else{VariableArray[0] = Math.tan(VariableArray[CurrentIndex]);}}";
-        }
-        if(AlexCodeInput[i] == "="){
-            EvalString += "VariableArray[CurrentIndex] = VariableArray[CopiedPointer];"
-        }
-        
-    }
-    document.getElementById('Output').value = "Output: \n";
-    eval(EvalString);
 };
-setInterval(function(){
-    let StringText = "";
-    for(let i = 0; i < VariableArray.length; i++){
-        StringText += "|";
-        if(CopiedPointer == i){
-            StringText += "*";
-        } 
-        else{
-            StringText += " ";
-        }
-        StringText += VariableArray[i];
+
+const UpdateBuffer = () => {
+    document.getElementById("Buffer").value = "";
+    for(let i = 0; i < BufferArray.length; i++){
+        document.getElementById("Buffer").value += "|";
         if(CurrentIndex == i){
-            StringText += "^";
+            document.getElementById("Buffer").value += "^";
         }
         else{
-            StringText == " ";
+            document.getElementById("Buffer").value += " ";
+        }
+        document.getElementById("Buffer").value += BufferArray[i];
+        if(CopiedIndex == i){
+            document.getElementById("Buffer").value += "*";
+        }
+        else{
+            document.getElementById("Buffer").value += " ";
         }
     }
-    document.getElementById("Variables").value = StringText;
-}, 10);
+};
+
+const Execute = (char, index) => {
+    if(char == "+"){
+        BufferArray[CurrentIndex]++;
+        UpdateBuffer();
+    }
+    if(char == "-"){
+        BufferArray[CurrentIndex]--;
+        UpdateBuffer();
+    }
+    if(char == "<"){
+        CurrentIndex--;
+        if(CurrentIndex == -1){
+            CurrentIndex = BufferArray.length;
+        }
+        UpdateBuffer();
+    }
+    if(char == ">"){
+        CurrentIndex++;
+        if(CurrentIndex == BufferArray.length){
+            CurrentIndex = 0;
+        }
+        UpdateBuffer();
+    }
+    if(char == "["){
+        if(BufferArray[CurrentIndex] == 0){
+            let NumberOfLoops = 0;
+            for(let i = CodeIndex; i < CodeString.length; i++){
+                if(CodeString[i] == "["){
+                    NumberOfLoops++;
+                }
+                if(CodeString[i] == "]"){
+                    NumberOfLoops--;
+                    if(NumberOfLoops == 0){
+                        CodeIndex = i;
+                        break;
+                    }
+                }
+            }
+        }
+        else{
+            OpenWhileLoopIDs.push(CodeIndex);
+        }
+    }
+    if(char == "]"){
+        if(OpenWhileLoopIDs[OpenWhileLoopIDs.length - 1] != undefined && ClosedWhileLoopIDs[OpenWhileLoopIDs.length - 1] != undefined){
+            if(BufferArray[CurrentIndex] == 0){
+                delete OpenWhileLoopIDs[OpenWhileLoopIDs.length - 1];
+                delete ClosedWhileLoopIDs[OpenWhileLoopIDs.length - 1];
+            }
+            else{
+                CodeIndex = OpenWhileLoopIDs[OpenWhileLoopIDs.length - 1];
+            }
+        }
+        else{
+            ClosedWhileLoopIDs.push(CurrentIndex);
+            CodeIndex = OpenWhileLoopIDs[OpenWhileLoopIDs.length - 1];
+        }
+    }
+    if(char == "."){
+        document.getElementById('Output').value += String.fromCharCode(BufferArray[CurrentIndex]);
+    }
+    if(char == ","){
+        BufferArray[CurrentIndex] = prompt().charCodeAt(0);
+        UpdateBuffer();
+    }
+    if(char == ","){
+        BufferArray[CurrentIndex] = prompt().charCodeAt(0);
+        UpdateBuffer();
+    }
+    if(char == "#"){
+        $("canvas").clearCanvas();
+    }
+    if(char == "@"){
+        Vertices++;
+        DrawObject["x" + Vertices] = BufferArray[CopiedIndex];
+        DrawObject["y" + Vertices] = BufferArray[CurrentIndex];
+    }
+    if(char == "%"){
+        if(CurrentDraw == "LINE"){
+            if(DrawObject["strokeStyle"] == undefined){
+                DrawObject["strokeStyle"] = "#";
+            }
+            DrawObject["strokeStyle"] += BufferArray[CurrentIndex];
+        }
+        else{
+            if(DrawObject["fillStyle"] == undefined){
+                DrawObject["fillStyle"] = "#";
+            }
+            DrawObject["fillStyle"] += BufferArray[CurrentIndex];
+        }
+    }
+    if(char == "&"){
+        DrawObject["strokeWidth"] = BufferArray[CurrentIndex];
+    }
+    if(char == "!"){
+        if(CurrentDraw == "LINE")
+            DrawObject["closed"] = true;
+            $("canvas").drawLine(DrawObject);
+        else{
+            $('canvas').drawPolygon(DrawObject);
+        }
+    }
+    if(char == "?"){
+        if(CurrentDraw == "LINE")
+            $("canvas").drawLine(DrawObject);
+        else{
+            $("canvas").drawPolygon(DrawObject);
+        }
+    }
+    if(char == "~"){
+        CopiedIndex = CurrentIndex;
+    }
+    if(char == "`"){
+
+    }
+    CodeIndex++;
+    if(CodeIndex + 1 <= $("#CodeInput").val().length){
+        setTimeout(function(){Execute($("#CodeInput").val()[CodeIndex], CodeIndex)}, 400);
+    }
+};
+
+$("#ActionButton").click((event) => {
+    Clear();
+    Execute($("#CodeInput").val()[CodeIndex], CodeIndex);
+});
