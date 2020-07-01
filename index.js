20d7a3817070c8153848a69495586dce8c262375
NASADeveloper102 <NASADeveloper102@gmail.com>
2020-07-01 17:09:05 -0400

    Added AlexBinary

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ let canvas = document.querySelector("#Canvas");
 let ctx = canvas.getContext('2d');
 let place = undefined;
 let lastKeyPressed = undefined;
-
+let binaryCommands = [];
 document.addEventListener('keydown', function(event){
     lastKeyPressed = event.which;
 });
@@ -29,6 +29,7 @@ const Clear = () => {
     CopiedIndex = undefined;
     CodeIndex = 0;
     BufferArray = [];
+    binaryCommands = [];
     ExSpeed = 400;
     ctx.fillStyle = "#000000";
     document.getElementById("Output").value = "";
@@ -59,7 +60,7 @@ const UpdateBuffer = () => {
     }
 };
 
-const Execute = (char, index) => {
+const ExecuteAlex = (char, index) => {
     if(char == "\\"){
         for(let i = CodeIndex; i < CodeString.length; i++){
             if(CodeString[i] == "\\"){
@@ -226,12 +227,194 @@ const Execute = (char, index) => {
     }
     CodeIndex++;
     if(CodeIndex + 1 <= CodeString.length){
-        setTimeout(function(){Execute(CodeString[CodeIndex], CodeIndex)}, ExSpeed);
+        setTimeout(function(){ExecuteAlex(CodeString[CodeIndex], CodeIndex)}, ExSpeed);
     }
 };
 
+
+
+
+const ExecuteBinary = (char, index) => {
+    if(char == "10000000"){
+        BufferArray[CurrentIndex]++;
+        UpdateBuffer();
+    }
+    if(char == "01000000"){
+        BufferArray[CurrentIndex]--;
+        UpdateBuffer();
+    }
+    if(char == "00100000"){
+        CurrentIndex--;
+        if(CurrentIndex == -1){
+            CurrentIndex = BufferArray.length;
+        }
+        UpdateBuffer();
+    }
+    if(char == "00010000"){
+        CurrentIndex++;
+        if(CurrentIndex == BufferArray.length){
+            CurrentIndex = 0;
+        }
+        UpdateBuffer();
+    }
+    if(char == "00000010"){
+        if(BufferArray[CurrentIndex] == 0){
+            let NumberOfLoops = 0;
+            for(let i = CodeIndex; i < CodeString.length; i++){
+                if(CodeString[i] == "["){
+                    NumberOfLoops++;
+                }
+                if(CodeString[i] == "]"){
+                    NumberOfLoops--;
+                    if(NumberOfLoops == 0){
+                        CodeIndex = i;
+                        break;
+                    }
+                }
+            }
+        }
+        else{
+            OpenWhileLoopIDs.push(CodeIndex);
+        }
+    }
+    if(char == "00000001"){
+        if(OpenWhileLoopIDs[OpenWhileLoopIDs.length - 1] != undefined && ClosedWhileLoopIDs[OpenWhileLoopIDs.length - 1] != undefined){
+            if(BufferArray[CurrentIndex] != 0){
+                delete OpenWhileLoopIDs[OpenWhileLoopIDs.length - 1];
+                delete ClosedWhileLoopIDs[OpenWhileLoopIDs.length - 1];
+            }
+            else{
+                CodeIndex = OpenWhileLoopIDs[OpenWhileLoopIDs.length - 1];
+            }
+        }
+        else{
+            ClosedWhileLoopIDs.push(CurrentIndex);
+            CodeIndex = OpenWhileLoopIDs[OpenWhileLoopIDs.length - 1];
+        }
+    }
+    if(char == "00000100"){
+        document.getElementById('Output').value += String.fromCharCode(BufferArray[CurrentIndex]);
+    }
+    if(char == "00001000"){
+        BufferArray[CurrentIndex] = prompt().charCodeAt(0);
+        UpdateBuffer();
+    }
+    if(char == "10000001"){
+        $("canvas").clearCanvas();
+    }
+    if(char == "00000000"){
+        BufferArray = [];
+        for (var i = 0; i < 10; i++) {
+            BufferArray.push(0);
+        }
+    }
+    if(char == "11000000"){
+        ctx.lineTo(BufferArray[CurrentIndex], BufferArray[CopiedIndex]);
+    }
+    if(char == "11000011"){
+        DrawObject.Colors.push(BufferArray[CurrentIndex]);
+    }
+    if(char == "W"){
+        ctx.lineWidth = BufferArray[CurrentIndex];
+    }
+    if(char == "S"){
+        ctx.strokeStyle = "rgb(" + DrawObject.Colors[0].toString() + ", " + DrawObject.Colors[1].toString() + ", " + DrawObject.Colors[2].toString() + ")";
+    }
+    if(char == "00010011"){
+        ctx.fillStyle = "rgb(" + DrawObject.Colors[0] + ", " + DrawObject.Colors[1] + ", " + DrawObject.Colors[2] + ")";
+    }
+    if(char == "00011100"){
+        ctx.beginPath();
+    }
+    if(char == "11100011"){
+        ctx.fill();
+    }
+    if(char == "01101000"){
+        ctx.moveTo(BufferArray[CurrentIndex], BufferArray[CopiedIndex]);
+    }
+    if(char == "00000111"){
+        CopiedIndex = CurrentIndex;
+    }
+    if(char == "10101010"){
+        ExSpeed = BufferArray[CurrentIndex];
+    }
+    if(char == "d"){
+        BufferArray[CurrentIndex] = BufferArray[CurrentIndex]/BufferArray[CopiedIndex];
+        UpdateBuffer();
+    }
+    if(char == "11011011"){
+        BufferArray[CurrentIndex] = BufferArray[CurrentIndex] * BufferArray[CopiedIndex];
+        UpdateBuffer();
+
+    }
+    if(char == "00100100"){
+        BufferArray[CurrentIndex] = Math.pow(BufferArray[CurrentIndex], 2);
+        console.log("oOFDSFDS");
+        UpdateBuffer();
+    }
+    if(char == '00011000'){
+        BufferArray[CurrentIndex] = Math.sqrt(BufferArray[CurrentIndex]);
+        UpdateBuffer();
+    }
+    if(char == "B"){
+        for(let i = 0; i < BufferArray[CurrentIndex]; i++){
+            BufferArray.push(0);
+        }
+        UpdateBuffer();
+    }
+    if(char == "11111111"){
+        BufferArray[CurrentIndex] = BufferArray[CopiedIndex];
+        UpdateBuffer();
+    }
+    if(char == ")"){
+        CodeIndex = place;
+    }
+    if(char == "("){
+        OpenFunctionIDs.push([CodeIndex, BufferArray[CurrentIndex]]);
+        for(let i = CodeIndex; i < CodeString.length; i++){
+            if(CodeString[i] == ")"){
+                ClosedFunctionIDs.push([i, BufferArray[CurrentIndex]]);
+                CodeIndex = i;
+            }
+        }
+    }
+    if(char == "R"){
+        let found = false;
+        for(let i = 0; i < OpenFunctionIDs.length; i++){
+            if(OpenFunctionIDs[i][1] == BufferArray[CurrentIndex] && found == false){
+                place = CodeIndex;
+                CodeIndex = OpenFunctionIDs[i][0];
+                found = true;
+            }
+        }
+    }
+    if(char == "k"){
+        BufferArray[CurrentIndex] = lastKeyPressed;
+        UpdateBuffer();
+    }
+    CodeIndex++;
+    if(CodeIndex + 1 <= binaryCommands.length){
+        setTimeout(function(){ExecuteBinary(binaryCommands[CodeIndex], CodeIndex)}, ExSpeed);
+    }
+};
 $("#ActionButton").click((event) => {
     Clear();
     CodeString = $("#CodeInput").val();
-    Execute(CodeInput[0], CodeIndex);
+    ExecuteAlex(CodeInput[0], CodeIndex);
 });
+
+$("#BinaryButton").click((event) => {
+    Clear();
+    CodeString = $("#CodeInput").val();
+    let StringToParse = "";
+    for( let i = 0 ; i < CodeString.length; i++){
+        if(CodeString[i] == "0" || CodeString[i] == "1"){
+            StringToParse += CodeString[i];
+        }
+    }
+    for(let i = 0; i < StringToParse.length/8; i++){
+        binaryCommands.push(StringToParse[ i * 8 ] + StringToParse[ i * 8 +1] + StringToParse[ i * 8 +2] + StringToParse[ i * 8 +3] + StringToParse[ i * 8 +4] + StringToParse[ i * 8 +5] + StringToParse[ i * 8 + 6] + StringToParse[ i * 8 + 7]);
+    }
+    ExecuteBinary(binaryCommands[0], CodeIndex);
+});
+
